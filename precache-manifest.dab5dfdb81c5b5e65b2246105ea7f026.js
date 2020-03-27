0a56a00fc947bd740cb2d351c910bdbff158d8fb
Jignesh Dalal <Jignesh.Dalal@ICERTIS.COM>
2020-03-27 16:53:00 +0530

    Updates

---
+++ precache-manifest.dab5dfdb81c5b5e65b2246105ea7f026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1844cb52526a6f70c413301772229dc8",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "a489f7a6399e87b66737",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e925cea9b895e6c3c8e8",
+    "url": "/react/static/js/2.22882890.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.22882890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a489f7a6399e87b66737",
+    "url": "/react/static/js/main.5faecddb.chunk.js"
+  },
+  {
+    "revision": "d125219f2c15929429b8",
+    "url": "/react/static/js/runtime-main.7325c9b3.js"
+  }
+]);
