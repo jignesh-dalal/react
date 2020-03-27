7c68f521ddfe97915482db64dd74df57da98094a
Jignesh Dalal <Jignesh.Dalal@ICERTIS.COM>
2020-03-27 19:08:01 +0530

    Modified workflow

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Checkbox List</h1>
+        <h1>Checkbox List using Render Props</h1>
         <Usage />
       </header>
     </div>
